128f3a7e53d415128629d980c4f8e9ad929987fc
Youngjune Jeong <jeong.yjune@gmail.com>
2024-05-26 18:45:14 +0900

    feat: add nav mesh volume, following player AI controller

---
+++ Source/SimpleShooter/ShooterAIController.cpp
@@ -0,0 +1,28 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ShooterAIController.h"
+
+#include "Kismet/GameplayStatics.h"
+
+void AShooterAIController::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void AShooterAIController::Tick(float DeltaSeconds)
+{
+	Super::Tick(DeltaSeconds);
+	APawn* PlayerPawn = UGameplayStatics::GetPlayerPawn(GetWorld(), 0);
+
+	if (LineOfSightTo(PlayerPawn))
+	{
+		SetFocus(PlayerPawn);
+		MoveToActor(PlayerPawn, 200);
+	}
+	else
+	{
+		ClearFocus(EAIFocusPriority::Gameplay);
+		StopMovement();
+	}
+}
