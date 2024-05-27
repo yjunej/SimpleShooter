eb5f053ea4491168da5548175860254e1bef0c54
Youngjune Jeong <jeong.yjune@gmail.com>
2024-05-27 14:16:15 +0900

    feat(UI): add healthbar

--- Source/SimpleShooter/ShooterCharacter.h
+++ Source/SimpleShooter/ShooterCharacter.h
@@ -26,6 +26,9 @@ public:
 	UFUNCTION(BlueprintPure)
 	bool IsDead() const;
 
+	UFUNCTION(BlueprintPure)
+	float GetHealthPercent() const;
+
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
