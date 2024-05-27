ae272b20027feb7887ca1c34ae465a5c0d3b547d
Youngjune Jeong <jeong.yjune@gmail.com>
2024-05-27 12:16:46 +0900

    feat(UI): add lose message widget blueprint

--- Source/SimpleShooter/ShooterPlayerController.cpp
+++ Source/SimpleShooter/ShooterPlayerController.cpp
@@ -3,10 +3,17 @@
 
 #include "ShooterPlayerController.h"
 #include "TimerManager.h"
+#include "Blueprint/UserWidget.h"
 
 void AShooterPlayerController::GameHasEnded(AActor* EndGameFocus, bool bIsWinner)
 {
 	Super::GameHasEnded(EndGameFocus, bIsWinner);
+	UUserWidget* LoseScreen = CreateWidget(this, LoseScreenClass);
+	if (LoseScreen != nullptr)
+	{
+		LoseScreen->AddToViewport();
+	}
 	GetWorldTimerManager().SetTimer(
 		RestartTimer, this, &APlayerController::RestartLevel, RestartDelay);
+
 }
