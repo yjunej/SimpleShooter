2c93fdb73b23df93421a52ec6cd3c85a4780836d
Youngjune Jeong <jeong.yjune@gmail.com>
2024-05-27 10:47:39 +0900

    feat: complete AI BehaviorTree - Chase, Shoot, Back to Start point

--- Source/SimpleShooter/ShooterAIController.cpp
+++ Source/SimpleShooter/ShooterAIController.cpp
@@ -23,13 +23,13 @@ void AShooterAIController::Tick(float DeltaSeconds)
 	Super::Tick(DeltaSeconds);
 	//APawn* PlayerPawn = UGameplayStatics::GetPlayerPawn(GetWorld(), 0);
 
-	if (LineOfSightTo(PlayerPawn))
-	{
-		GetBlackboardComponent()->SetValueAsVector(TEXT("PlayerLocation"), PlayerPawn->GetActorLocation());
-		GetBlackboardComponent()->SetValueAsVector(TEXT("LastKnownPlayerLocation"), PlayerPawn->GetActorLocation());	
-	}
-	else
-	{
-		GetBlackboardComponent()->ClearValue(TEXT("PlayerLocation"));
-	}
+	//if (LineOfSightTo(PlayerPawn))
+	//{
+	//	GetBlackboardComponent()->SetValueAsVector(TEXT("PlayerLocation"), PlayerPawn->GetActorLocation());
+	//	GetBlackboardComponent()->SetValueAsVector(TEXT("LastKnownPlayerLocation"), PlayerPawn->GetActorLocation());	
+	//}
+	//else
+	//{
+	//	GetBlackboardComponent()->ClearValue(TEXT("PlayerLocation"));
+	//}
 }
